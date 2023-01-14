31bda3ea56608fa9605e9d10a5cec6d19dcf4703
Richard Arends <richard@unixguru.nl>
2023-01-14 13:55:21 +0100

    Install Rocky Linux using Terraform

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rockylinux-terraform
-Install Rocky Linux ona local machine with Libvirt and Terraform
+Install Rocky Linux on a local machine with Libvirt and Terraform
+
+Read my [blog post](https://blog.mosibi.nl/all/2020/06/07/terraform-centos8-libvirt.html) on creating a CentOS 8 system using Terraform and the libvirt provider. Although the blog post describes Centos 8, everything also applies to Rocky Linux.
---
+++ main.tf
@@ -0,0 +1,81 @@
+variable "vm_name" {
+  type = string
+  default = "my-test-vm"
+}
+
+variable "domain" {
+  type = string
+  default = "example.com"
+}
+
+variable "memory" {
+  type = string
+  default = "2048"
+}
+
+variable "cpu" {
+  type = number
+  default = 2
+}
+
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+
+
+resource "libvirt_volume" "qcow_volume" {
+  name = "${var.vm_name}.img"
+  pool = "default"
+  source = "https://dl.rockylinux.org/pub/rocky/8/images/x86_64/Rocky-8-GenericCloud-Base.latest.x86_64.qcow2"
+  format = "qcow2"
+}
+
+data "template_file" "user_data" {
+  template = file("${path.module}/cloud_init.cfg")
+
+  vars = {
+   hostname = var.vm_name
+   domain = var.domain
+  }
+}
+
+resource "libvirt_cloudinit_disk" "commoninit" {
+  name           = "commoninit.iso"
+  user_data      = data.template_file.user_data.rendered
+  pool           = "default"
+}
+
+# Define KVM domain to create
+resource "libvirt_domain" "kvm_domain" {
+  name   = var.vm_name
+  memory = var.memory
+  vcpu   = var.cpu
+
+  cloudinit = libvirt_cloudinit_disk.commoninit.id
+
+  disk {
+    volume_id = libvirt_volume.qcow_volume.id
+  }
+
+  console {
+    type = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type = "spice"
+    listen_type = "address"
+    autoport = true
+  }
+
+  network_interface {
+    network_name = "default"
+    wait_for_lease = true
+  }
+}
+
+output "ip" {
+  value = libvirt_domain.kvm_domain.network_interface.0.addresses.0
+}
+
